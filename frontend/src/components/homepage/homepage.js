335ad1c6354395a822a2a138321b7e2c58250219
Ethan McCullagh <ethanmccullagh@w134-87-056-136.wireless.uvic.ca>
2023-11-07 14:57:49 -0800

    Fixed Homepage

--- frontend/src/components/homepage/homepage.js
+++ frontend/src/components/homepage/homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./homepage.css";
 import Item from "./items";
+import logo from "./medLogo.svg"
 
 function Homepage() {
   const items = [
@@ -8,10 +9,19 @@ function Homepage() {
     { id: 2, feature: "Triage" },
   ];
 
-  return <div class="wrap">
-    <div class="board"><Item feature ={items[0].feature }> </Item></div>
-    <div class="board"><Item feature ={items[1].feature }> </Item></div>
+  return <div className="HomePage">
+    <header className="Home-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+          Mister ED
+      </p>
+    </header>
+    <div className="square-list">
+      <div class="board"><Item feature ={items[0].feature }> </Item></div>
+      <div class="board"><Item feature ={items[1].feature }> </Item></div>
+    </div>
   </div>
+
 }
 
 export default Homepage;
