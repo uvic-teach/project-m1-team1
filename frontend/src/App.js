53d4fb50f97ac8f99b5b0edf491a0ce69e467752
AmannSingh <amann.singh7@outlook.com>
2023-11-07 03:32:07 -0800

    Added routing to homepage and userAuthentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-import Login from './components/login/login';
-import Homepage from './components/homepage/homepage'
+import "./App.css";
+import Login from "./components/login/login";
+import Homepage from "./components/homepage/homepage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import ErrorPage from "./error_page";
+import { AuthProtectedRoutes, useAuth } from "./context/AuthContext";
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      
+      <Route path="/" element={<Login />} errorElement={<ErrorPage/>}/>
+      <Route element={<AuthProtectedRoutes/>} >
+        <Route path="/home" element={<Homepage />} errorElement={<ErrorPage/>} />
+      </Route>
+    </Route>
+  )
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,85 +1,105 @@
 import React from "react";
-import './login.css';
-import logo from './medLogo.svg';
+import "./login.css";
+import logo from "./medLogo.svg";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
+import { useAuth } from "../../context/AuthContext";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Login() {
+  const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth();
+  const [UserName, setUserName] = useState("");
+  const [Password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const [UserName, setUserName] = useState("");
-    const [Password, setPassword] = useState("");
+  function UserNameHandler(event) {
+    setUserName(event.target.value);
+  }
 
-    function UserNameHandler(event) {
-        setUserName(event.target.value)
+  function PasswordHandler(event) {
+    setPassword(event.target.value);
+  }
+
+  function PrintUserName() {
+    console.log(UserName);
+  }
+
+  var loginInfo = {
+    username: "",
+    password: "",
+  };
+
+  async function loginRequest() {
+    loginInfo.username = UserName;
+    loginInfo.password = Password;
+
+   const response = await axios
+      .post("https://seng350-team1-auth.azurewebsites.net/login", loginInfo)
+      
+    
+    if(response?.status === 200){
+        setIsLoggedIn(true);
+        setAuthUser({
+            username: UserName,
+            authToken: response.data['auth_token']
+
+        })
+
+        console.log(authUser)
     }
 
-    function PasswordHandler(event) {
-        setPassword(event.target.value)
+    if(location.state?.from){
+        navigate(location.state.from);
     }
 
-    function PrintUserName() {
-        console.log(UserName)
-    }
+      
+  }
 
-    var loginInfo = {
-        "username": "",
-        "password": ""
-    }
-
-    async function loginRequest() {
-        loginInfo.username = UserName
-        loginInfo.password = Password
-
-        axios.post('https://seng350-team1-auth.azurewebsites.net/login', loginInfo)
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-            })
-    }
-
-    return (
-        <div>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Mister ED
-                </p>
-                <form>
-                    <label>Username</label>
-                    <input type="text" id="fname" name="fname" value={UserName} onChange={UserNameHandler} />
-                </form>
-                <form>
-                    <label>Password</label>
-                    <input type="text" id="fname" name="fname" value={Password} onChange={PasswordHandler} />
-                </form>
-                <a
-                    className="App-link"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={loginRequest}
-                >
-                    <button
-                        className="Login-Button"
-                    >
-                        Login
-                    </button>
-                </a>
-                <a
-                    className="App-link"
-                    href="./LoginPage.js"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <button
-                        className="Register-Button"
-                    >
-                        Register
-                    </button>
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Mister ED</p>
+        <form>
+          <label>Username</label>
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            value={UserName}
+            onChange={UserNameHandler}
+          />
+        </form>
+        <form>
+          <label>Password</label>
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            value={Password}
+            onChange={PasswordHandler}
+          />
+        </form>
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={loginRequest}
+        >
+          <button className="Login-Button">Login</button>
+        </a>
+        <a
+          className="App-link"
+          href="./LoginPage.js"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="Register-Button">Register</button>
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default Login;
-
