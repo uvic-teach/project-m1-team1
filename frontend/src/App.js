2b65e376114dff53d398f4adbdfcca9ffe8c8c39
Ethan McCullagh <ethanmccullagh@Ethans-MacBook-Air.local>
2023-11-12 10:06:22 -0800

    started on triage page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Login from './components/login/login';
 import Homepage from './components/homepage/homepage';
 import WaitList from './components/waitlist/waitlist';
+import Triage from './components/triage/triage';
 
 
 function App() {
   return (
     <div className="App">
-      <WaitList />
+      <Triage />
     </div>
   );
 }
